817664254ac7fbdaf323c10c3fe0e92c4f7eb97a
Niels G. W. Serup <ngws@metanohi.name>
2024-04-02 20:17:00 +0200

    Kopiér kode fra https://api.arcade.academy/en/latest/examples/platform_tutorial/step_01.html og få det til at køre

---
+++ python-arcade-test/README.md
@@ -0,0 +1,3 @@
+# python-arcade-test
+
+Niels prøver at følge <https://api.arcade.academy/en/latest/examples/platform_tutorial/index.html#platformer-tutorial>.
---
+++ python-arcade-test/shell.nix
@@ -0,0 +1,11 @@
+# Use this file with nix-shell or similar tools; see https://nixos.org/
+with import <nixpkgs> {};
+
+mkShell {
+  buildInputs = [ python3 ];
+
+  shellHook = ''
+    export LD_LIBRARY_PATH="${pkgs.libglvnd}/lib:${pkgs.xorg.libX11}/lib:${pkgs.freetype}/lib";
+    . .venv/bin/activate
+  '';
+}
