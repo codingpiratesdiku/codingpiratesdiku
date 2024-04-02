04104bf5b8ee584f4c0b9d96ca8d42062d695565
Niels G. W. Serup <ngws@metanohi.name>
2024-04-02 20:19:36 +0200

    Kortere kode

--- python-arcade-test/test.py
+++ python-arcade-test/test.py
@@ -1,23 +1,13 @@
 #!/usr/bin/env python3
 
-"""
-Platformer Game
-"""
 import arcade
 
-# Constants
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
 SCREEN_TITLE = "Platformer"
 
-
 class MyGame(arcade.Window):
-    """
-    Main application class.
-    """
-
     def __init__(self):
-
         # Call the parent class and set up the window
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
@@ -29,17 +19,9 @@ class MyGame(arcade.Window):
 
     def on_draw(self):
         """Render the screen."""
-
         self.clear()
-        # Code to draw the screen goes here
 
 
-def main():
-    """Main function"""
-    window = MyGame()
-    window.setup()
-    arcade.run()
-
-
-if __name__ == "__main__":
-    main()
+window = MyGame()
+window.setup()
+arcade.run()
