54fff6160b3278d967f3c4f25297845d482982fc
Niels G. W. Serup <ngws@metanohi.name>
2024-04-03 21:05:04 +0200

    Følg https://api.arcade.academy/en/latest/examples/platform_tutorial/step_02.html

--- python-arcade-test/test.py
+++ python-arcade-test/test.py
@@ -6,21 +6,57 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
 SCREEN_TITLE = "Platformer"
 
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
+
 class MyGame(arcade.Window):
     def __init__(self):
         # Call the parent class and set up the window
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
+        self.wall_list = None
+        self.player_list = None
+        self.player_sprite = None
+
         arcade.set_background_color(arcade.csscolor.OLIVE)
 
     def setup(self):
         """Set up the game here. Call this function to restart the game."""
-        pass
+        self.player_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+
+        # Set up the player, specifically placing it at these coordinates.
+        image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+        self.player_list.append(self.player_sprite)
+
+        # Create the ground
+        # This shows using a loop to place multiple sprites horizontally
+        for x in range(0, 1250, 64):
+            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
+            wall.center_x = x
+            wall.center_y = 32
+            self.wall_list.append(wall)
+
+        # Put some crates on the ground
+        # This shows using a coordinate list to place sprites
+        coordinate_list = [[512, 96], [256, 96], [768, 96]]
+
+        for coordinate in coordinate_list:
+            # Add a crate on the ground
+            wall = arcade.Sprite(
+                ":resources:images/tiles/boxCrate_double.png", TILE_SCALING
+            )
+            wall.position = coordinate
+            self.wall_list.append(wall)
 
     def on_draw(self):
         """Render the screen."""
         self.clear()
-
+        self.wall_list.draw()
+        self.player_list.draw()
 
 window = MyGame()
 window.setup()
